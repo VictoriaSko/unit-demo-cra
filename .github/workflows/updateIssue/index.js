66c6edb27cd59e88b632b9c4318ee0e48b05b67a
Victoria <victoria.sko@fora-soft.com>
2023-07-16 20:25:47 +0300

    chore(task-3): update issue logic

--- .github/workflows/updateIssue/index.js
+++ .github/workflows/updateIssue/index.js
@@ -5,7 +5,8 @@ const updateIssue = async () => {
     const tagName = core.getInput("tag_name", { required: true });
     const owner = core.getInput("owner", { required: true });
     const repo = core.getInput("repo", { required: true });
-    const testsResult = core.getInput("testsResult", { required: false });
+    const testsResult = core.getInput("tests_result", { required: false });
+    const testsResultUrl = core.getInput("tests_result_url", { required: false });
 
     const octokit = github.getOctokit(core.getInput("github_token", { required: true }));
 
@@ -16,9 +17,20 @@ const updateIssue = async () => {
     if (!issueNumber) return;
 
     if (testsResult) {
+        const testObject = JSON.parse(testsResult);
         const commentBody = `# Результат запуска проверок
 
-    ${testsResult}
+        ## Проверка Lint:
+        ${testObject.lint.result}
+
+        ## Запуск Jest тестов:
+        ${testObject["unit-tests"].result}
+
+        ## Запуск Playwright тестов:
+        ${testObject["e2e-tests"].result}
+
+        ## Ссылка на результат:
+        ${testsResultUrl}
     `;
 
         await octokit.rest.issues.createComment({ owner, repo, issue_number: issueNumber, body: commentBody });
