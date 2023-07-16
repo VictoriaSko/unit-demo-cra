ef8462ba868b28b988e55de1dccaa911afd2e72f
Victoria <victoria.sko@fora-soft.com>
2023-07-16 18:28:56 +0300

    chore(task-3): add @actions to release workflow

--- .github/workflows/createIssue/dist/index.js
+++ .github/workflows/createIssue/dist/index.js
@@ -9798,12 +9798,19 @@ const github = __nccwpck_require__(5438);
 const createIssue = async () => {
     const octokit = github.getOctokit(core.getInput("github_token", {required: true}));
 
+    const changelog = core.getInput("changelog", {required: true});
+
+    const issueBody = `# Test body
+    Test text <br>
+
+    ${changelog.replace('%OA', '\n')}`;
+
     await octokit.rest.issues.create({
         owner: core.getInput("owner", {required: true}),
         repo: core.getInput("repo", {required: true}),
         title: `RELEASE - ${core.getInput("tag_name", {required: true})}`,
         labels: ["RELEASE"],
-        body: "#Test body\n*Test text*",
+        body: issueBody,
     });
 };
 
--- .github/workflows/createIssue/index.js
+++ .github/workflows/createIssue/index.js
@@ -4,12 +4,19 @@ const github = require('@actions/github');
 const createIssue = async () => {
     const octokit = github.getOctokit(core.getInput("github_token", {required: true}));
 
+    const changelog = core.getInput("changelog", {required: true});
+
+    const issueBody = `# Test body
+    Test text <br>
+
+    ${changelog.replace('%OA', '\n')}`;
+
     await octokit.rest.issues.create({
         owner: core.getInput("owner", {required: true}),
         repo: core.getInput("repo", {required: true}),
         title: `RELEASE - ${core.getInput("tag_name", {required: true})}`,
         labels: ["RELEASE"],
-        body: "#Test body\n*Test text*",
+        body: issueBody,
     });
 };
 
