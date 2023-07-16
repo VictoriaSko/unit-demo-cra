b9e5cb7ddf29616df1bfeafb1714b3b30e2f4a24
Victoria <victoria.sko@fora-soft.com>
2023-07-16 19:24:03 +0300

    chore(task-3): change release actions

--- .github/workflows/createIssue/dist/index.js
+++ .github/workflows/createIssue/dist/index.js
@@ -9796,25 +9796,37 @@ const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
 const createIssue = async () => {
+    const tagName = core.getInput("tag_name", { required: true });
+    const actor = core.getInput("actor", { required: true });
+    const owner = core.getInput("owner", { required: true });
+    const repo = core.getInput("repo", { required: true });
+
     const octokit = github.getOctokit(core.getInput("github_token", { required: true }));
+
+    const release = await octokit.rest.repos.getReleaseByTag({
+        owner,
+        repo,
+        tagName,
+    });
 
     const changelog = core.getInput("changelog", { required: false }) || "";
 
     const formattedChangelog = changelog ? `## Changelog
-
-${changelog.replace('%OA', '\n')}
+${changelog.replaceAll('%OA', '\n')}
     ` : "";
 
-    const issueBody = `# Test body
-Test text
+    const issueBody = `# Релиз ${tagName}
+Дата релиза: ${release.created_at}
+Автор релиза: ${actor}
+Версия релиза: ${tagName}
 
 ${formattedChangelog}
 `;
 
     await octokit.rest.issues.create({
-        owner: core.getInput("owner", { required: true }),
+        owner, 
         repo: core.getInput("repo", { required: true }),
-        title: `RELEASE - ${core.getInput("tag_name", { required: true })}`,
+        title: `RELEASE - ${tagName}`,
         labels: ["RELEASE"],
         body: issueBody,
     });
--- .github/workflows/createIssue/index.js
+++ .github/workflows/createIssue/index.js
@@ -2,25 +2,37 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const createIssue = async () => {
+    const tagName = core.getInput("tag_name", { required: true });
+    const actor = core.getInput("actor", { required: true });
+    const owner = core.getInput("owner", { required: true });
+    const repo = core.getInput("repo", { required: true });
+
     const octokit = github.getOctokit(core.getInput("github_token", { required: true }));
+
+    const release = await octokit.rest.repos.getReleaseByTag({
+        owner,
+        repo,
+        tagName,
+    });
 
     const changelog = core.getInput("changelog", { required: false }) || "";
 
     const formattedChangelog = changelog ? `## Changelog
-
-${changelog.replace('%OA', '\n')}
+${changelog.replaceAll('%OA', '\n')}
     ` : "";
 
-    const issueBody = `# Test body
-Test text
+    const issueBody = `# Релиз ${tagName}
+Дата релиза: ${release.created_at}
+Автор релиза: ${actor}
+Версия релиза: ${tagName}
 
 ${formattedChangelog}
 `;
 
     await octokit.rest.issues.create({
-        owner: core.getInput("owner", { required: true }),
+        owner, 
         repo: core.getInput("repo", { required: true }),
-        title: `RELEASE - ${core.getInput("tag_name", { required: true })}`,
+        title: `RELEASE - ${tagName}`,
         labels: ["RELEASE"],
         body: issueBody,
     });
